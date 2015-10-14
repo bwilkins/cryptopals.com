d96d6c4f1e6d13e9725c11de5ec1513e2df6a7fb
Brett Wilkins <brett@brett.geek.nz>
2015-10-15 09:01:40 +1100

    Adding challenge 6 WIP

---
+++ sets/1/challenges/6.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/env ruby
+
+require 'base64'
+
+input_file = File.expand_path('../6.txt', __FILE__)
+input = File.read(input_file)
+b64decoded = Base64.decode64(input)
+bytea = b64decoded.bytes
+
+RANKING = "ETAOIN SHRDLU"
+
+def hamming_distance(s1, s2)
+  if s1.size != s2.size
+    puts "sizeof #{s1} != sizeof #{s2}"
+    return nil
+  end
+
+  s1.zip(s2).inject(0) do |sum, (c1, c2)|
+    val = c1 ^ c2
+    while (val != 0)
+      sum += 1
+      val &= val - 1
+    end
+    sum
+  end
+end
+
+def xor_bytea_byte(bytea, byte)
+  bytea.map { |b| b ^ byte }
+end
+
+def bytea_to_hexstr(bytea)
+  bytea.map{|b|"%02x"%b}.join
+end
+
+def rank_permutation(permutation)
+  RANKING.chars.each_with_index.inject(0) do |sum, (char, index)|
+    weight = RANKING.size - index
+    sum += weight * permutation.count(char)
+  end
+end
+
+def bytea_to_str(bytea)
+  bytea.map(&:chr).join
+end
+
+def find_most_likely_decoded_sequence(b1)
+  permutations = (0..255).map do |byte|
+    bytea_to_str(xor_bytea_byte(b1, byte)).upcase
+  end
+
+  ranked = permutations.each_with_index.inject({}) do |set, (perm, index)|
+    set.tap do |s|
+    s[rank_permutation(perm)] = {byte: index,permutation: perm}
+    end
+  end
+
+  ranked[ranked.keys.max]
+end
+
+class RepeatingXorKey
+  def initialize(key)
+    @key = key.bytes
+  end
+
+  def crypt(bytea)
+    bytea.map { |b| b ^ byte }
+  end
+
+  def byte
+    b = @key.first
+    @key.rotate!
+    return b
+  end
+end
+
+
+keysize_options = (2..40).inject({}) do |memo, keysize|
+  bytea1 = bytea[0, keysize]
+  bytea2 = bytea[keysize, keysize]
+
+  dist = hamming_distance(bytea1, bytea2)
+  norm = dist / keysize
+
+  memo[norm] ||= Array.new
+  memo[norm] << keysize
+
+  memo
+end
+
+keysize_select = keysize_options.keys.min
+keysize = keysize_options[keysize_select].first
+
+blocks_of_keysize = bytea.each_slice(keysize).to_a
+if (blocks_of_keysize.length % keysize) != 0
+  # Cut the length of to be fully divisible
+end
+columns = blocks_of_keysize.transpose
+
+key_bytea = columns.map do |c|
+  find_most_likely_decoded_sequence(c)[:byte]
+end
+
+key = bytea_to_str(key_bytea)
+
+xor_key = RepeatingXorKey.new(key)
+puts xor_key.crypt(bytea)
